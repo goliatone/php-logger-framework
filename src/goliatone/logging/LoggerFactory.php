67dab4bc5e1924d905554e547f0d2b84ca32cb4e
goliatone <burgosemi@gmail.com>
2013-10-07 19:30:35 -0400

    Added LoggerFactory.

---
+++ src/goliatone/logging/LoggerFactory.php
@@ -0,0 +1,14 @@
+<?php
+namespace goliatone\logging;
+
+use goliatone\logging\core\ILoggerFactory;
+
+/**
+ * 
+ */
+class LoggerFactory implements ILoggerFactory {
+	
+	function __construct($argument) {
+		
+	}
+}

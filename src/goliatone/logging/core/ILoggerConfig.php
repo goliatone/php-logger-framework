6f2c27a48f63370f6b05f8b978b7cb007c29799a
goliatone <burgosemi@gmail.com>
2013-10-11 14:17:54 -0400

    Core: Added ILoggerConfig.

---
+++ src/goliatone/logging/core/ILoggerConfig.php
@@ -0,0 +1,78 @@
+<?php
+
+
+interface ILoggerConfig
+{
+    /**
+     *
+     * @param   id
+     */
+    public function setId( /*:String*/$id );
+
+    /**
+     * TODO: We have different deserializers => YAML/JSON/DDBB/XML/ETC
+     * @param   xml
+     */
+    public function fromXML( $xml );
+
+    /**
+     *
+     * @param   mode
+     */
+    public function setMode( /*:String*/$mode );
+
+    /**
+     *
+     */
+    public function reset( );
+
+    /**
+     *
+     */
+    public function getManagerPackage( )/*:String*/;
+
+    /**
+     *
+     */
+    public function getThreshold()/*:String*/;
+
+    /**
+     *
+     */
+    public function getPublishers()/*:Array*/;
+
+    /**
+     *
+     */
+    public function getLevelFilters()/*:Array*/;
+
+    /**
+     *
+     */
+    public function getDisabledLevels()/*:Array*/;
+
+    /**
+     *
+     */
+    public function getDisabledPackages()/*:Array*/;
+
+    /**
+     *
+     */
+    public function getDebugger()/*:String*/;
+
+    /**
+     *
+     */
+    public function getLogger()/*:String*/;
+
+    /**
+     *
+     */
+    public function getNotNullDebugger()/*:Boolean*/;
+
+    /**
+     *
+     */
+    public function getNotNullLogger()/*:Boolean*/;
+}
